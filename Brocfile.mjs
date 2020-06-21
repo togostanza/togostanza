0361e6e12109e54b55832ff5c0cdafab5db8f947
Keita Urashima <ursm@ursm.jp>
2020-06-22 03:51:43 +0900

    Broccoli
    TODO: livereload

---
+++ Brocfile.mjs
@@ -0,0 +1,113 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import BroccoliPlugin from 'broccoli-plugin';
+import Handlebars from 'handlebars';
+import RSVP from 'rsvp';
+import RollupCommonjs from '@rollup/plugin-commonjs';
+import RollupResolve from '@rollup/plugin-node-resolve';
+import glob from 'fast-glob';
+import { rollup } from 'rollup';
+
+import { packagePath } from './util.mjs';
+
+async function handlebarsTemplate(fpath, opts = {}) {
+  const hbs = await fs.readFile(fpath, 'utf8');
+
+  return Handlebars.compile(hbs, opts);
+}
+
+class BuildStanza extends BroccoliPlugin {
+  async build() {
+    const stanzas = this.allStanzas;
+
+    await Promise.all([
+      this.buildIndex(stanzas),
+      this.buildStanzaLib(),
+      this.buildStanzas(stanzas)
+    ]);
+  }
+
+  async buildIndex(stanzas) {
+    const template = await handlebarsTemplate(packagePath('index.html.hbs'));
+    const metadata = await Promise.all(stanzas.map(({metadata}) => metadata));
+
+    this.output.writeFileSync('index.html', template({
+      stanzas: metadata
+    }));
+  }
+
+  async buildStanzaLib() {
+    const bundle = await rollup({
+      input: packagePath('stanza.js'),
+
+      plugins: [
+        RollupResolve.default(),
+        RollupCommonjs()
+      ]
+    });
+
+    const {output} = await bundle.generate({format: 'esm'});
+
+    this.output.writeFileSync('stanza.js', output[0].code);
+  }
+
+  async buildStanzas(stanzas) {
+    const templates = await RSVP.hash({
+      entrypoint: handlebarsTemplate(packagePath('entrypoint.js.hbs'), {noEscape: true}),
+      help:       handlebarsTemplate(packagePath('help.html.hbs'))
+    });
+
+    await Promise.all(stanzas.map(stanza => this.buildStanza(stanza, templates)));
+  }
+
+  async buildStanza(stanza, templates) {
+    const metadata = await stanza.metadata;
+
+    this.output.writeFileSync(`${stanza.id}.js`, templates.entrypoint({
+      metadataJSON: JSON.stringify(metadata),
+      script:       await stanza.script,
+      templates:    await stanza.templates,
+      outerJSON:    JSON.stringify(await stanza.outer)
+    }));
+
+    this.output.writeFileSync(`${stanza.id}.html`, templates.help({metadata}));
+  }
+
+  get allStanzas() {
+    return this.inputPaths.flatMap((providerDir) => {
+      return glob.sync(`${providerDir}/*/metadata.json`).map((metadataPath) => {
+        const stanzaDir = path.dirname(metadataPath);
+
+        return {
+          id: path.basename(stanzaDir),
+
+          get metadata() {
+            return fs.readFile(metadataPath).then(JSON.parse);
+          },
+
+          get script() {
+            return fs.readFile(path.join(stanzaDir, 'index.js'), 'utf8');
+          },
+
+          get templates() {
+            const paths = glob.sync('templates/*.html', {cwd: stanzaDir});
+
+            return Promise.all(paths.map(async (templatePath) => {
+              return {
+                name: path.basename(templatePath),
+                spec: Handlebars.precompile(await fs.readFile(path.join(stanzaDir, templatePath), 'utf8'))
+              };
+            }));
+          },
+
+          get outer() {
+            return fs.readFile(path.join(stanzaDir, '_header.html'), 'utf8').catch(() => null);
+          }
+        };
+      });
+    });
+  }
+}
+
+export default () => new BuildStanza(['.']);
--- cli.mjs
+++ cli.mjs
@@ -1,5 +1,76 @@
 #!/usr/bin/env node
 
-import defaultTask from './gulpfile.mjs';
+import TreeSync from 'tree-sync';
+import UI from 'console-ui';
+import broccoli from 'broccoli';
+import messages from 'broccoli/dist/messages.js';
 
-defaultTask();
+import brocfile from './Brocfile.mjs';
+
+const ui      = new UI();
+const builder = new broccoli.Builder(brocfile());
+
+const watcher = new broccoli.Watcher(builder, builder.watchedSourceNodeWrappers, {
+  saneOptions: {
+    ignored: 'dist/**'
+  }
+});
+
+switch (process.argv[2]) {
+  case undefined:
+  case 'serve':
+    broccoli.server.serve(
+      watcher,
+      'localhost',
+      8080,
+      undefined,
+      undefined,
+      ui
+    );
+
+    break;
+  case 'build':
+    build(builder, watcher, ui, {watch: false});
+    break;
+  case 'watch':
+    build(builder, watcher, ui, {watch: true});
+    break;
+  default:
+    ui.writeLine('togostanza serve|build|watch');
+}
+
+async function build(builder, watcher, ui, {watch}) {
+  const outputTree = new TreeSync(builder.outputPath, 'dist');
+
+  watcher.on('buildSuccess', () => {
+    outputTree.sync();
+    messages.default.onBuildSuccess(builder, ui);
+
+    if (!watch) {
+      watcher.quit();
+    }
+  });
+
+  watcher.on('buildFailure', (e) => {
+    ui.writeLine('build failure', 'ERROR');
+    ui.writeError(e);
+  });
+
+  process.on('SIGINT',  () => watcher.quit());
+  process.on('SIGTERM', () => watcher.quit());
+
+  try {
+    await watcher.start()
+  } catch (e) {
+    ui.writeError(e);
+  } finally {
+    try {
+      builder.cleanup();
+      process.exit(0);
+    } catch (e) {
+      ui.writeLine('cleanup error', 'ERROR');
+      ui.writeError(e);
+      process.exit(1);
+    }
+  }
+}
