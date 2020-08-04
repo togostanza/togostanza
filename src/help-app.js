bf9ac203370ebdcba85dc4c0d763a807e79334b7
Keita Urashima <ursm@ursm.jp>
2020-08-04 17:27:29 +0900

    Remove debug print

--- src/help-app.js
+++ src/help-app.js
@@ -2,9 +2,5 @@ import { createApp } from 'vue';
 import Help from './Help.vue';
 
 export default function(metadata) {
-  console.log(metadata);
-
-  return createApp(Help, {
-    metadata
-  })
+  return createApp(Help, {metadata})
 };
