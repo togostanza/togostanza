f231452c58df1e169a2ba439cba027c8ee88512f
Yoji Shidara <dara@shidara.net>
2020-08-20 07:25:53 +0900

    Ignore hash variation of Layout.js on test

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -140,7 +140,8 @@ describe('build', () => {
         ignore: [
           'node_modules'
         ]
-      })).toMatchSnapshot();
+      }).map(fn => fn.replace(/^(-togostanza\/Layout-)[0-9a-f]+(\.js(?:\.map))?$/, '$1HASH$2')
+      )).toMatchSnapshot();
 
       expect(fs.readFileSync('dist/hello.js', 'utf8')).toContain('function hello(stanza, params) {');
     });
