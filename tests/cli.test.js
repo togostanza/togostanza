5d8df6f4dda3218450ace2d03005adeec1cec76d
Yoji Shidara <dara@shidara.net>
2020-08-20 08:11:41 +0900

    Fix regexp

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -140,7 +140,7 @@ describe('build', () => {
         ignore: [
           'node_modules'
         ]
-      }).map(fn => fn.replace(/^(-togostanza\/Layout-)[0-9a-f]+(\.js(?:\.map))?$/, '$1HASH$2')
+      }).map(fn => fn.replace(/^(-togostanza\/Layout-)[0-9a-f]+(\.js(?:\.map)?)$/, '$1HASH$2')
       )).toMatchSnapshot();
 
       expect(fs.readFileSync('dist/hello.js', 'utf8')).toContain('function hello(stanza, params) {');
