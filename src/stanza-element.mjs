04751ab55f2a5f24e42cc5a33bc5c86eea11b0ea
Yoji Shidara <dara@shidara.net>
2020-08-19 12:22:00 +0900

    Insert icon

--- src/stanza-element.mjs
+++ src/stanza-element.mjs
@@ -106,6 +106,7 @@ class AboutLinkElement extends HTMLElement {
       background-color: white;
       opacity: 0.5;
       transition: opacity 0.2s ease-in-out;
+      line-height: 16px;
     }
 
     :host(:hover) {
@@ -116,10 +117,20 @@ class AboutLinkElement extends HTMLElement {
       text-decoration: none;
       padding: 0.2rem 0.5rem;
     }
+
+    .label {
+      display: none;
+    }
+
+    :host(:hover) .label {
+      display: inline;
+    }
     `;
 
+    const infoIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm6.5-.25A.75.75 0 017.25 7h1a.75.75 0 01.75.75v2.75h.25a.75.75 0 010 1.5h-2a.75.75 0 010-1.5h.25v-2h-.25a.75.75 0 01-.75-.75zM8 6a1 1 0 100-2 1 1 0 000 2z"></path></svg>`;
+
     const anchor = document.createElement("a");
-    anchor.innerHTML = this.innerHTML || "About this stanza";
+    anchor.innerHTML = this.innerHTML || infoIcon + '<span class="label">About this stanza</span>';
     anchor.href = href;
 
     this.shadowRoot.appendChild(style);
