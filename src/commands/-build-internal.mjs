0beb1a410ab8664f7af9faf27af1a7eda15ed132
Keita Urashima <ursm@ursm.jp>
2020-09-09 11:23:32 +0900

    Watch togostanza system files on developing togostanza

--- src/commands/-build-internal.mjs
+++ src/commands/-build-internal.mjs
@@ -1,11 +1,14 @@
+import fs from 'fs';
 import path from 'path';
 
 import Funnel from 'broccoli-funnel'
 import MergeTrees from 'broccoli-merge-trees';
 import UI from 'console-ui';
 import broccoli from 'broccoli';
+import debounce from 'lodash.debounce';
 import messages from 'broccoli/dist/messages.js';
 import picomatch from 'picomatch';
+import sane from 'sane';
 
 import BuildStanza from '../build-stanza.mjs';
 import BundleStanzaModules from '../bundle-stanza-modules.mjs';
@@ -45,6 +48,8 @@ export async function runWatcher(repositoryDir, builder, onBuildSuccess = () => 
   process.on('SIGINT',  () => watcher.quit());
   process.on('SIGTERM', () => watcher.quit());
 
+  watchPackageFiles(repositoryDir, builder);
+
   try {
     await watcher.start();
   } finally {
@@ -52,6 +57,32 @@ export async function runWatcher(repositoryDir, builder, onBuildSuccess = () => 
   }
 
   process.exit(0); // perhaps LiveServer is still listening and needs to stop the process
+}
+
+import resolve from 'resolve';
+
+function watchPackageFiles(repositoryDir, builder) {
+  const packageJsonPath = resolve.sync('togostanza/package.json', {basedir: repositoryDir});
+  const installedPath   = path.resolve(packageJsonPath, '..');
+
+  if (!fs.lstatSync(installedPath).isSymbolicLink()) { return; }
+
+  const watcher = sane(fs.realpathSync(installedPath), {glob: ['**/*']});
+  const ui = new UI();
+
+  const handler = debounce(async () => {
+    try {
+      await builder.build();
+      messages.default.onBuildSuccess(builder, ui);
+    } catch (e) {
+      ui.writeLine('build failure', 'ERROR');
+      ui.writeError(e);
+    }
+  }, 100);
+
+  watcher.on('add',    handler);
+  watcher.on('delete', handler);
+  watcher.on('change', handler);
 }
 
 export function composeTree(repositoryDir, {environment}) {
