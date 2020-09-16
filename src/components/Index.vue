3091b87786a11b2a2e26e6946624922adf503b65
Keita Urashima <ursm@ursm.jp>
2020-09-16 14:26:34 +0900

    Improve style of index page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,10 +1,11 @@
 <template>
   <Layout>
-    <h1>List of Stanzas</h1>
+    <h1 class="display-4">List of Stanzas</h1>
 
     <div class="list-group mt-3">
       <a v-for="metadata in allMetadata" :key="metadata['@id']" :href="`./${metadata['@id']}.html`" class="list-group-item list-group-item-action py-3">
-        {{metadata['stanza:label']}}
+        <div>{{metadata['stanza:label']}}</div>
+        <p class="small text-muted text-truncate mt-1 mb-0">{{metadata['stanza:definition']}}</p>
       </a>
     </div>
   </Layout>
