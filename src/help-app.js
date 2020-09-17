838c1ab53bc8a3a42dec70017197587d286cb3d5
Keita Urashima <ursm@ursm.jp>
2020-09-17 15:53:59 +0900

    Don't worry about the order of the arguments

--- src/help-app.js
+++ src/help-app.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import Help from './components/Help.vue';
 
-export default function(metadata, readme) {
+export default function({metadata, readme}) {
   return createApp(Help, {metadata, readme})
 };
