4dd337b4912989d94f1be1f0653211fcdb9b8c90
Keita Urashima <ursm@ursm.jp>
2020-10-01 13:17:12 +0900

    Show message when no stanzas defined

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,12 +2,14 @@
   <Layout>
     <h1 class="display-4">List of Stanzas</h1>
 
-    <div class="list-group mt-3">
+    <div v-if="allMetadata.length > 0" class="list-group mt-3">
       <a v-for="metadata in allMetadata" :key="metadata['@id']" :href="`./${metadata['@id']}.html`" class="list-group-item list-group-item-action py-3">
         <div>{{metadata['stanza:label']}}</div>
         <p class="small text-muted text-truncate mt-1 mb-0">{{metadata['stanza:definition']}}</p>
       </a>
     </div>
+
+    <p v-else>No stanzas defined.</p>
   </Layout>
 </template>
 
