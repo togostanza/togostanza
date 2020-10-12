ada136fe59740a9b20dd9f1206f759cae799942f
Keita Urashima <ursm@ursm.jp>
2020-10-12 11:57:17 +0900

    Update sample code

--- src/generators/stanza/templates/index.js
+++ src/generators/stanza/templates/index.js
@@ -1,10 +1,8 @@
 export default async function <%= camelCase(id) %>(stanza, params) {
-  const sayTo = params['say-to'] || 'world';
-
   stanza.render({
     template: 'stanza.html.hbs',
     parameters: {
-      greeting: `Hello, ${sayTo}!`
+      greeting: `Hello, ${params['say-to']}!`
     }
   });
 }
