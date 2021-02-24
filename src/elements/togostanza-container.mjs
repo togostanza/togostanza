fe329273921d761bd36b1e072ac303b7da1864ef
Yoji Shidara <dara@shidara.net>
2021-02-24 10:53:29 +0900

    WIP

--- src/elements/togostanza-container.mjs
+++ src/elements/togostanza-container.mjs
@@ -9,29 +9,39 @@ export default class ContainerElement extends HTMLElement {
   connectedCallback() {
     console.log("ContainerElement connectedCallback");
     setTimeout(() => {
-    const stanzaElements = Array.from(this.querySelectorAll("*")).filter(el => el.tagName.startsWith("TOGOSTANZA-"));
-    for (const srcEl of stanzaElements) {
+      const stanzaElements = Array.from(
+        this.querySelectorAll("*")
+      ).filter((el) => el.tagName.startsWith("TOGOSTANZA-"));
+
+      // src -> container
+      const eventNames = new Set();
+      for (const srcEl of stanzaElements) {
         console.log("EV", srcEl, srcEl.stanza);
         for (const eventName of srcEl.stanza.metadata.eventNames || []) {
+          eventNames.add(eventName);
+          // TODO skip if event is already registers
           srcEl.addEventListener(eventName, (event) => {
             const clonedEvent = new event.constructor(event.type, event);
+            clonedEvent.srcElement = srcEl;
             this.dispatchEvent(clonedEvent);
           });
-          console.log("REGISTERING", eventName, srcEl)
-            this.addEventListener(eventName, (event) => {
-              console.log("RECEIVED EVENT ON CONTAINER", event);
-                for (const destEl of stanzaElements) {
-                  // TODO skip srcEl === destEl
-                    console.log("BROADCASTING", event)
-                    //destEl.addEventListener(clonedEvent);
-                    destEl.stanzaModule.handleEvent(event);
-                }
-            })
         }
-    }}, 0);
-  }
+      }
 
-  foo = "bar";
+      // container -> dest
+      for (const eventName of eventNames) {
+        // destEl の metadata を見て必要なイベントだけをhandleさせる
+        console.log("container->dest", eventName);
+        this.addEventListener(eventName, (event) => {
+          for (const destEl of stanzaElements) {
+            // TODO skip srcEl === destEl
+            console.log("BROADCASTING", event);
+            destEl.stanza.handleEvent(event);
+          }
+        });
+      }
+    }, 0);
+  }
 }
 
 ContainerElement.customElementName = "togostanza-container";
