5a167cc129969dca9769790b3d6f2df7c41f5235
Yoji Shidara <dara@shidara.net>
2021-03-04 15:39:08 +0900

    Introduce togostanza-event-map

--- src/elements/togostanza-container.mjs
+++ src/elements/togostanza-container.mjs
@@ -1,25 +1,78 @@
+import get from 'lodash.get';
+
+function connectStanzasWithHandler(stanzaElements) {
+  for (const srcEl of stanzaElements) {
+    for (const eventName of outgoingEventNames(srcEl.stanza)) {
+      srcEl.addEventListener(eventName, (event) => {
+        for (const destEl of stanzaElements) {
+          if (incomingEventNames(srcEl.stanza).includes(eventName)) {
+            destEl.stanza.handleEvent(event);
+          }
+        }
+      });
+    }
+  }
+}
+
+function connectStanzasWithAttributes(container, stanzaElements) {
+  for (const mapElement of container.querySelectorAll('togostanza-event-map')) {
+    const on              = mapElement.getAttribute('on');
+    const receiver        = mapElement.getAttribute('receiver');
+    const targetAttribute = mapElement.getAttribute('target-attribute');
+    const valuePath       = mapElement.getAttribute('value-path');
+
+    const receiverElements = container.querySelectorAll(receiver);
+
+    for (const srcEl of stanzaElements) {
+      if (!outgoingEventNames(srcEl.stanza).includes(on)) { continue; }
+
+      srcEl.addEventListener(on, (event) => {
+        const value = get(event.detail, valuePath);
+        if (value === true) {
+          setEach(receiverElements, targetAttribute, targetAttribute);
+        } else if (value === false || value === undefined) {
+          removeEach(receiverElements, targetAttribute);
+        } else if (value instanceof String) {
+          setEach(receiverElements, targetAttribute, value);
+        } else {
+          setEach(receiverElements, targetAttribute, JSON.stringify(value));
+        }
+      });
+    }
+  }
+}
+
 export default class ContainerElement extends HTMLElement {
   connectedCallback() {
-    setTimeout(() => {
-      const stanzaElements = Array.from(
-        this.querySelectorAll('*')
-      ).filter((el) => el.tagName.startsWith('TOGOSTANZA-') && 'stanza' in el);
+    const stanzaElements = Array.from(
+      this.querySelectorAll('*')
+    ).filter((el) => el.tagName.startsWith('TOGOSTANZA-') && 'stanza' in el);
 
-      for (const srcEl of stanzaElements) {
-        const outgoingEventNames = srcEl.stanza.metadata['stanza:outgoingEvent']?.map(e => e['stanza:key']) || [];
-        for (const eventName of outgoingEventNames) {
-          srcEl.addEventListener(eventName, (event) => {
-            for (const destEl of stanzaElements) {
-              const incomingEventNames = srcEl.stanza.metadata['stanza:incomingEvent']?.map(e => e['stanza:key']) || [];
-              if (incomingEventNames.includes(eventName)) {
-                destEl.stanza.handleEvent(event);
-              }
-            }
-          });
-        }
-      }
+    setTimeout(() => { // wait until stanzas ready
+      connectStanzasWithAttributes(this, stanzaElements);
+      connectStanzasWithHandler(stanzaElements)
     }, 0);
   }
 }
 
 ContainerElement.customElementName = 'togostanza-container';
+
+function setEach(elements, key, value) {
+  for (const el of elements) {
+    el.setAttribute(key, value);
+  }
+}
+
+function removeEach(elements, key) {
+  for (const el of elements) {
+    el.removeAttribute(key);
+  }
+}
+
+function outgoingEventNames(stanza) {
+  return stanza.metadata['stanza:outgoingEvent']?.map(e => e['stanza:key']) || [];
+}
+
+function incomingEventNames(stanza) {
+  return stanza.metadata['stanza:incomingEvent']?.map(e => e['stanza:key']) || [];
+}
