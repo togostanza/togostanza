f6cc50bf30f09033a3f83026cf4b0d2c978a93db
Yoji Shidara <dara@shidara.net>
2021-03-15 16:49:11 +0900

    Use the latest bootstrap css

--- src/elements/togostanza--menu.mjs
+++ src/elements/togostanza--menu.mjs
@@ -85,10 +85,11 @@ export default class MenuElement extends LitElement {
       }
     }
 
+    // TODO stop using CDN
     return html`<link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
         rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
         crossorigin="anonymous"
       />
 
