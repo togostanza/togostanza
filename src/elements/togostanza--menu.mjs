ab54cb76b8f40a1faf49eaa0dfb9a9e75e82a7cf
Yoji Shidara <dara@shidara.net>
2021-03-15 16:33:19 +0900

    WIP: somehow works

--- src/elements/togostanza--menu.mjs
+++ src/elements/togostanza--menu.mjs
@@ -1,16 +1,22 @@
-import { info } from "@primer/octicons";
+import * as octicons from "@primer/octicons";
 import { LitElement, css, html } from "lit-element";
 import { unsafeSVG } from "lit-html/directives/unsafe-svg";
 import Dropdown from "bootstrap/js/dist/dropdown";
 
 export default class MenuElement extends LitElement {
-  dropdown = null;
-  dropdownOpen = false;
+  constructor() {
+    super();
+    this.dropdownOpen = false;
+    this.dropdown = null;
+    this.menuItems = [];
+  }
 
   static get properties() {
     return {
       placement: { type: String },
       href: { type: String },
+      dropdownOpen: { type: Boolean, attribute: false },
+      menuItems: { type: Array, attribute: false },
     };
   }
 
@@ -22,8 +28,10 @@ export default class MenuElement extends LitElement {
         opacity: 0.5;
         transition: opacity 0.2s ease-in-out;
 
-        bottom: 0;
+        top: initial;
         right: 0;
+        bottom: 0;
+        left: initial;
       }
 
       :host(:hover) {
@@ -41,15 +49,21 @@ export default class MenuElement extends LitElement {
 
       :host([placement="top-left"]) {
         top: 0;
+        right: initial;
+        bottom: initial;
         left: 0;
       }
 
       :host([placement="top-right"]) {
         top: 0;
         right: 0;
+        bottom: initial;
+        left: initial;
       }
 
       :host([placement="bottom-left"]) {
+        top: initial;
+        right: initial;
         bottom: 0;
         left: 0;
       }
@@ -60,20 +74,15 @@ export default class MenuElement extends LitElement {
     `;
   }
 
-  render() {
-    function handleClick(e) {
-      console.log(e);
-      // this.dropdown?.hide();
-    }
+  item(label, handler) {
+    this.menuItems.push({ type: "item", label, handler });
+  }
 
-    function handleToggle(e) {
-      console.log(e);
+  render() {
+    function handleClick() {
       if (this.dropdownOpen) {
         this.dropdown?.hide();
-      } else {
-        this.dropdown?.show();
       }
-      this.dropdownOpen = !this.dropdownOpen;
     }
 
     return html`<link
@@ -83,47 +92,49 @@ export default class MenuElement extends LitElement {
         crossorigin="anonymous"
       />
 
-      <div class="dropdown">
-        <button
-          id="dLabel"
-          type="button"
-          aria-expanded="false"
-          class="btn btn-default dropdown-toggle"
-          @click=${handleToggle}
-        >
-          Dropdown trigger
-        </button>
-        <ul class="dropdown-menu" @click=${handleClick}>
+      <div class="dropdown" @click=${handleClick}>
+        <a aria-expanded="false" class="kebab-icon">
+          ${unsafeSVG(octicons["kebab-horizontal"].toSVG({ width: 16 }))}
+        </a>
+        <ul class="dropdown-menu">
+          ${this.menuItems.map((item) => {
+            switch (item.type) {
+              case "item":
+                return html`<li>
+                  <a class="dropdown-item" @click=${item.handler}
+                    >${item.label}</a
+                  >
+                </li>`;
+              case "divider":
+                return html`<li><hr class="dropdown-divider" /></li>`;
+            }
+          })}
+          ${this.menuItems.length > 0
+            ? html`<li>
+                <hr class="dropdown-divider" />
+              </li>`
+            : ""}
           <li>
-            <a class="dropdown-item">Another action</a>
-          </li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-          <li><hr class="dropdown-divider" /></li>
-          <li>
-            <a class="dropdown-item" href=${this.href}>About this stanza</a>
+            <a
+              class="dropdown-item"
+              href=${this.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              >About this stanza</a
+            >
           </li>
         </ul>
       </div>`;
   }
 
   firstUpdated() {
-    // console.log(this.shadowRoot.querySelector(".dropdown-menu"));
-    this.dropdown = new Dropdown(
-      this.shadowRoot.querySelector(".dropdown-toggle")
-    );
-
-    const d = this.shadowRoot.querySelector(".dropdown");
-    d.addEventListener("show.bs.dropdown", () => {
-      console.log("show");
+    this.dropdown = new Dropdown(this.shadowRoot.querySelector(".kebab-icon"));
+    const dd = this.shadowRoot.querySelector(".dropdown");
+    dd.addEventListener("shown.bs.dropdown", (e) => {
+      this.dropdownOpen = true;
     });
-    d.addEventListener("shown.bs.dropdown", () => {
-      console.log("shown");
-    });
-    d.addEventListener("hide.bs.dropdown", () => {
-      console.log("hide");
-    });
-    d.addEventListener("hidden.bs.dropdown", () => {
-      console.log("hidden");
+    dd.addEventListener("hidden.bs.dropdown", (e) => {
+      this.dropdownOpen = false;
     });
   }
 }
