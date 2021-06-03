57809fecd1c9eba29ba47870e9ce82d4c5b951c2
Yoji Shidara <dara@shidara.net>
2021-06-03 11:14:21 +0900

    Get container to work with class-based stanza

--- src/elements/togostanza--container.mjs
+++ src/elements/togostanza--container.mjs
@@ -1,14 +1,14 @@
-import get from 'lodash.get';
+import get from "lodash.get";
 
 export default class ContainerElement extends HTMLElement {
   dataSourceUrls = {};
 
   connectedCallback() {
-    setTimeout(() => { // wait until stanzas ready
-      const stanzaElements = Array.from(
-        this.querySelectorAll('*')
-      ).filter((el) => el.tagName.startsWith('TOGOSTANZA-') && 'stanza' in el);
-
+    setTimeout(() => {
+      // wait until stanzas ready
+      const stanzaElements = Array.from(this.querySelectorAll("*")).filter(
+        (el) => el.tagName.startsWith("TOGOSTANZA-") && "stanzaInstance" in el
+      );
       connectStanzasWithAttributes(this, stanzaElements);
       connectStanzasWithHandler(stanzaElements);
       connectDataSource(this);
@@ -43,12 +43,12 @@ export default class ContainerElement extends HTMLElement {
       return entry.value;
     }
 
-    const blob      = await fetch(url).then(res => res.blob());
+    const blob = await fetch(url).then((res) => res.blob());
     const objectUrl = URL.createObjectURL(blob);
 
     this.dataSourceUrls[url] = {
       value: objectUrl,
-      count: 1
+      count: 1,
     };
 
     return objectUrl;
@@ -57,7 +57,9 @@ export default class ContainerElement extends HTMLElement {
   disposeDataSourceUrl(url) {
     const entry = this.dataSourceUrls[url];
 
-    if (!entry) { return; }
+    if (!entry) {
+      return;
+    }
 
     entry.count--;
 
@@ -69,15 +71,15 @@ export default class ContainerElement extends HTMLElement {
   }
 }
 
-ContainerElement.customElementName = 'togostanza--container';
+ContainerElement.customElementName = "togostanza--container";
 
 function connectStanzasWithHandler(stanzaElements) {
   for (const srcEl of stanzaElements) {
-    for (const eventName of outgoingEventNames(srcEl.stanza)) {
+    for (const eventName of outgoingEventNames(srcEl.stanzaInstance)) {
       srcEl.addEventListener(eventName, (event) => {
         for (const destEl of stanzaElements) {
-          if (incomingEventNames(destEl.stanza).includes(eventName)) {
-            destEl.stanza.handleEvent(event);
+          if (incomingEventNames(destEl.stanzaInstance).includes(eventName)) {
+            destEl.stanzaInstance.handleEvent(event);
           }
         }
       });
@@ -86,25 +88,30 @@ function connectStanzasWithHandler(stanzaElements) {
 }
 
 function connectStanzasWithAttributes(container, stanzaElements) {
-  for (const mapElement of container.querySelectorAll('togostanza--event-map')) {
-    const on              = mapElement.getAttribute('on');
-    const receiver        = mapElement.getAttribute('receiver');
-    const targetAttribute = mapElement.getAttribute('target-attribute');
-    const valuePath       = mapElement.getAttribute('value-path');
+  for (const mapElement of container.querySelectorAll(
+    "togostanza--event-map"
+  )) {
+    const on = mapElement.getAttribute("on");
+    const receiver = mapElement.getAttribute("receiver");
+    const targetAttribute = mapElement.getAttribute("target-attribute");
+    const valuePath = mapElement.getAttribute("value-path");
 
     const receiverElements = container.querySelectorAll(receiver);
 
     for (const srcEl of stanzaElements) {
-      if (!outgoingEventNames(srcEl.stanza).includes(on)) { continue; }
+      if (!outgoingEventNames(srcEl.stanzaInstance).includes(on)) {
+        continue;
+      }
 
       srcEl.addEventListener(on, (event) => {
         const value = valuePath ? get(event.detail, valuePath) : event.detail;
 
         if (value === true) {
-          setEach(receiverElements, targetAttribute, '');
+          setEach(receiverElements, targetAttribute, "");
         } else if (value === false || value === undefined) {
           removeEach(receiverElements, targetAttribute);
-        } else if (value.constructor === String) { // a bit weird, but a unified way to determine string literals and objects
+        } else if (value.constructor === String) {
+          // a bit weird, but a unified way to determine string literals and objects
           setEach(receiverElements, targetAttribute, value);
         } else {
           setEach(receiverElements, targetAttribute, JSON.stringify(value));
@@ -115,12 +122,14 @@ function connectStanzasWithAttributes(container, stanzaElements) {
 }
 
 function connectDataSource(container) {
-  for (const dataSource of container.querySelectorAll('togostanza--data-source')) {
+  for (const dataSource of container.querySelectorAll(
+    "togostanza--data-source"
+  )) {
     dataSource.containerElement = container;
 
-    const url             = dataSource.getAttribute('url');
-    const receiver        = dataSource.getAttribute('receiver');
-    const targetAttribute = dataSource.getAttribute('target-attribute');
+    const url = dataSource.getAttribute("url");
+    const receiver = dataSource.getAttribute("receiver");
+    const targetAttribute = dataSource.getAttribute("target-attribute");
 
     container.dataSourceUrlChanged(null, url, receiver, targetAttribute);
   }
@@ -138,10 +147,14 @@ function removeEach(elements, key) {
   }
 }
 
-function outgoingEventNames(stanza) {
-  return stanza.metadata['stanza:outgoingEvent']?.map(e => e['stanza:key']) || [];
+function outgoingEventNames(stanzaInstance) {
+  return (
+    stanzaInstance.metadata["stanza:outgoingEvent"]?.map((e) => e["stanza:key"]) || []
+  );
 }
 
-function incomingEventNames(stanza) {
-  return stanza.metadata['stanza:incomingEvent']?.map(e => e['stanza:key']) || [];
+function incomingEventNames(stanzaInstance) {
+  return (
+    stanzaInstance.metadata["stanza:incomingEvent"]?.map((e) => e["stanza:key"]) || []
+  );
 }
