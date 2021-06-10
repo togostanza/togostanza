50737762c0efcdd55fe4fe616ab44946bb999566
Yoji Shidara <dara@shidara.net>
2021-06-10 10:31:28 +0900

    WIP: try to convert to class-based stanza

--- tests/jscodeshift-playground.test.js
+++ tests/jscodeshift-playground.test.js
@@ -1,27 +1,62 @@
 import { defineInlineTest } from 'jscodeshift/dist/testUtils';
 
-defineInlineTest((file, api) => {
-  const j = api.jscodeshift;
-  const root = j(file.source);
+defineInlineTest(
+  (file, api) => {
+    const j = api.jscodeshift;
+    const root = j(file.source);
 
-  const callExpressions = root.find(j.CallExpression, {
-    callee: {
-      type: 'Identifier',
-      name: 'foo'
-    }
-  });
+    const callExpressions = root.find(j.CallExpression, {
+      callee: {
+        type: 'Identifier',
+        name: 'foo',
+      },
+    });
 
-  callExpressions.replaceWith((path) => {
-    const {params, body} = path.node.arguments[0];
+    callExpressions.replaceWith((path) => {
+      const { params, body } = path.node.arguments[0];
 
-    return j.exportDefaultDeclaration(
-      j.functionDeclaration(
-        j.identifier('foo'),
-        params,
-        body
-      )
-    );
-  });
+      return j.exportDefaultDeclaration(
+        j.functionDeclaration(j.identifier('foo'), params, body)
+      );
+    });
 
-  return root.toSource();
-}, {}, 'foo(function(bar) { baz; });', 'export default function foo(bar) { baz; };');
+    return root.toSource();
+  },
+  {},
+  'foo(function(bar) { baz; });',
+  'export default function foo(bar) { baz; };'
+);
+
+defineInlineTest(
+  (file, api) => {
+    const j = api.jscodeshift;
+    const root = j(file.source);
+
+    root.find(j.ExportDefaultDeclaration).replaceWith((path) => {
+      const functionDecl = path.node.declaration;
+      const body = functionDecl.body;
+
+      const fnExpr = j.functionExpression(null, [], body);
+      fnExpr.async = functionDecl.async; // NOTE workaround: j.functionExpression doesn't handle async argument
+
+      return j.exportDefaultDeclaration(
+        j.classDeclaration(
+          null,
+          j.classBody([
+            j.methodDefinition('method', j.identifier('render'), fnExpr),
+          ]),
+          j.identifier('Stanza')
+        )
+      );
+    });
+
+    return root.toSource();
+  },
+  {},
+  `export default function foo(stanza, params) { foo; };
+function fn() { expr };`,
+  `export default class extends Stanza {
+  render() { foo; }
+};
+function fn() { expr };`
+);
